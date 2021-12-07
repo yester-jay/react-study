a9deceb0d012365d10475a0675f3e8f1e091b8e0
jay.doh <jay.doh@kakaocorp.com>
2021-12-08 00:37:06 +0900

    lecture: 검색폼2

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -1,4 +1,4 @@
-import { qs } from "../helpers.js";
+import { qs, on } from "../helpers.js";
 import View from "./View.js";
 
 const tag = "[SearchFormView]";
@@ -9,13 +9,24 @@ export default class SearchFormView extends View {
 
     super(qs("#search-form-view"));
 
+    this.inputElement = qs("[type=text]", this.element);
     this.resetElement = qs("[type=reset]", this.element);
 
     this.showResetButton(false);
-    // TODO
+    this.bindEvent();
   }
 
   showResetButton(visible = true) {
     this.resetElement.style.display = visible ? "block" : "none";
+  }
+
+  bindEvent () {
+    on(this.inputElement, "keyup", () => this.handleKeyup())
+  }
+
+  handleKeyup () {
+    console.log(tag, "handleKeyup", this.inputElement.value);
+    const { value } = this.inputElement;
+    this.showResetButton(value.length > 0);
   }
 }
